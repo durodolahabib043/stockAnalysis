c53a628b3226682252fe549e17f73a7cb2515b12
durodolahabib043 <durodolahabib043@gmail.com>
2019-11-13 07:13:53 -0500

    added tensor flow

--- StockAnalysis.py
+++ StockAnalysis.py
@@ -4,22 +4,25 @@ import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
+import tensorflow as tf
 
-
-boeingDataFrame = pd.read_csv('/Users/hdurodola/Downloads/BoeingStock.csv')
-cocacolaDataFrame = pd.read_csv('/Users/hdurodola/Downloads/CocaColaStock.csv')
-geDataFrame = pd.read_csv('/Users/hdurodola/Downloads/GEStock.csv')
-ibmDataFrame = pd.read_csv('/Users/hdurodola/Downloads/IBMStock.csv')
-pgDataFrame = pd.read_csv('/Users/hdurodola/Downloads/ProcterGambleStock.csv')
+# /Users/habibdurodola/Documents/stockAnalysis/stockAnalysis/ProcterGambleStock.csv
+boeingDataFrame = pd.read_csv('/Users/habibdurodola/Documents/stockAnalysis/stockAnalysis/BoeingStock.csv')
+cocacolaDataFrame = pd.read_csv('/Users/habibdurodola/Documents/stockAnalysis/stockAnalysis/CocaColaStock.csv')
+geDataFrame = pd.read_csv('/Users/habibdurodola/Documents/stockAnalysis/stockAnalysis/GEStock.csv')
+ibmDataFrame = pd.read_csv('/Users/habibdurodola/Documents/stockAnalysis/stockAnalysis/IBMStock.csv')
+pgDataFrame = pd.read_csv('/Users/habibdurodola/Documents/stockAnalysis/stockAnalysis/ProcterGambleStock.csv')
 
 # convert date to string
 boeingDataFrame['Date'] = pd.to_datetime(boeingDataFrame['Date'], format='%d/%m/%y')
+
 
 print boeingDataFrame.head()
 
 #Data Visualization
 fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(12,5))
 sns.pointplot(data=boeingDataFrame, x="Date", y="StockPrice", ax=ax1)
+sns.pointplot(data=boeingDataFrame, x="Date" ,y="StockPrice" , ax=ax2)
 
 plt.show()
 
